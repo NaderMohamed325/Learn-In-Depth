a0ec33e12c77dd27a756fbdd668be03b69591af6
NaderMohamed325 <nadoorclasher@gmail.com>
2024-05-04 20:49:49 +0300

    keil lec3

---
+++ Unit 3/Lec 3/automation.sh
@@ -0,0 +1,4 @@
+make clean
+echo "waiting............."
+sleep 2
+make all
--- Unit 3/Lec 3/main.c
+++ Unit 3/Lec 3/main.c
@@ -1,10 +1,10 @@
 #define RCC_BASE_ADDR    0x40021000
-#define GPIOA_BASE_ADDR  0x40010800
+#define GPIOC_BASE_ADDR  0x40011000
 
 #define RCC_APB2ENR      (*(volatile unsigned int*)(RCC_BASE_ADDR + 0x18))
-#define GPIOA_CRH        (*(volatile unsigned int*)(GPIOA_BASE_ADDR + 0x04))
-#define GPIOA_ODR        (*(volatile unsigned int*)(GPIOA_BASE_ADDR + 0x0C))
-#define RCC_EN           (1 << 2)
+#define GPIOC_CRH        (*(volatile unsigned int*)(GPIOC_BASE_ADDR + 0x04))
+#define GPIOC_ODR        (*(volatile unsigned int*)(GPIOC_BASE_ADDR + 0x0C))
+#define RCC_EN           (1 << 4) // GPIOC clock enable bit
 #define LED_PIN          13
 
 void delay(int milliseconds) {
@@ -15,20 +15,20 @@ void delay(int milliseconds) {
 }
 
 int main() {
-    // Enable clock for GPIOA
+    // Enable clock for GPIOC
     RCC_APB2ENR |= RCC_EN;
 
     // Configure pin as output (push-pull, max speed 2 MHz)
-    GPIOA_CRH &= ~(0xF << ((LED_PIN - 8) * 4));
-    GPIOA_CRH |= (0x2 << ((LED_PIN - 8) * 4));
+    GPIOC_CRH &= ~(0xF << ((LED_PIN - 8) * 4));
+    GPIOC_CRH |= (0x2 << ((LED_PIN - 8) * 4));
 
     while (1) {
         // Turn LED on
-        GPIOA_ODR |= (1 << LED_PIN);
+        GPIOC_ODR |= (1 << LED_PIN);
         delay(1000);  // Delay 1 second
 
         // Turn LED off
-        GPIOA_ODR &= ~(1 << LED_PIN);
+        GPIOC_ODR &= ~(1 << LED_PIN);
         delay(1000);  // Delay 1 second
     }
 
